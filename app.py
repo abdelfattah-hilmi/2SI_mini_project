e6c3cb3ce2c01887b5d1bf777a6287d69fb74542
Abdelfattah <hilmiabdelfattah@gmail.com>
2022-03-22 23:35:15 +0100

    waiting for design

--- app.py
+++ app.py
@@ -7,13 +7,18 @@ app = Dash(__name__)
 
 
 app.layout = html.Div(children=[
-    html.H1(
-        className="title",
-        children = ["Intgeration check"]
+    html.Div(
+        className ="Center-card",
+        children = [
+            html.H1(
+            className = "title",
+            children = ["Intgeration check"]
+            ),
+            html.Br(),
+            html.H4('Place holder')
+        ]
         )
-    ]
-    
-)
+    ])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
